f517d45826f239c9f735f44dcaf577bbfbef7c93
Jonmainhart <jonmainhart@gmail.com>
2022-04-11 12:05:38 +0200

    Updated main file
    Added doctring.
    Added boilerplate.

---
+++ Steganosaurus/steganosaurus/__main__.py
@@ -0,0 +1,20 @@
+#! python3
+# __main__.py
+    """
+    Steganosaurus
+    
+    Linden Crandall
+    Jonthan Mainhart
+    Zhihua Zheng
+
+    Final Project for CMSC 495.
+
+    April 11, 2022
+
+    Main file for Steganosaurus.
+
+    Steganosaurus is an image-based steganography application written in Python.
+    """
+
+    if __name__ == "__main__":
+        pass
