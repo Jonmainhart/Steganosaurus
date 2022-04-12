77dfda2a28aa627c42ae9fdacd2159384569c90f
Zhihua Zheng <zhihuazheng2012@gmail.com>
2022-04-13 00:43:47 +0200

    Added main gui

--- Steganosaurus/steganosaurus/stego.py
+++ Steganosaurus/steganosaurus/stego.py
@@ -13,24 +13,56 @@ MainFrame classes for Steganosaurus.
 """
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.gridlayout  import GridLayout
 from kivy.lang import Builder
 from kivy.config import Config
 from kivy.factory import Factory
+from kivy.properties import StringProperty, BooleanProperty
+
+from utils import open_image
 
 # Set window size.
 Config.set('graphics', 'width', '550')
 Config.set('graphics', 'height', '500')
+Config.set('graphics', 'resizable', False)
 # Import external kv file.
 Builder.load_file('dialog.kv')
 
-class MainWidget(Widget):
+class MainWidget(GridLayout):
+
+    reset_btn_disabled = BooleanProperty(True)
+    user_notification_msg = StringProperty('Display Text Field Related Warning Message')
+    textfield_str = StringProperty('')
+    maximum_char_count = StringProperty('100')
 
     def popup_user_notification(self, message, message_type):
+
         # TODO: Replace with validating message_type
-        if self.get_id(message) == '"temporary_btn_id1"':
+        if self.get_id(message) == '"temporary_btn_id2"':
+            App.get_running_app().message = 'Warning Message!'
+            App.get_running_app().message_type = 'Warning'
             Factory.WarningPopup().open()
         else:
+            App.get_running_app().message = 'Info/Error Message'
+            App.get_running_app().message_type = 'Info/Error'
             Factory.InfoAndErrorPopup().open()
+        #print(str(MainFrame().message))
+
+    def on_open_button_click(self):
+        # pass
+        open_image()
+
+    def on_encode_button_click(self):
+        # TODO: If encode button is clicked/done
+        # reset_btn_disabled = BooleanProperty(False)
+        pass
+
+    def on_save_button_click(self):
+        pass
+        # print("Save Image button is clicked")
+
+    def on_reset_button_click(self):
+        print("Reset Image button is clicked")
 
     # Temporary function to get widget id from mainframe.
     # To display different popup windows.
@@ -40,10 +72,9 @@ class MainWidget(Widget):
                 return id
 
 class MainFrame(App):
-
+    message = 'message'
+    message_type = 'message_type'
     def build(self):
         self.title = 'Steganosaurus' # GUI title.
-        self.message = 'message' # TODO: Reset the variable.
-        self.message_type = 'message_type' # TODO: Reset the variable.
         return MainWidget()
 MainFrame().run()
